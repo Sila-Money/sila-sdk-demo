2d906953628a5136f6febdfc019e26797534cd90
Anthony Mack <amack300@Anthonys-MacBook-Pro-2.local>
2021-05-13 15:09:36 -0700

    SILA-2086 fix redirect when switching users

--- src/components/layout/NavbarUsers.js
+++ src/components/layout/NavbarUsers.js
@@ -19,17 +19,17 @@ const NavbarUsers = () => {
 
   const setActiveUser = (handle) => {
     const activeUser = app.users.find(u => u.handle === handle);
-    const flow = activeUser.flow || app.settings.flow;
     setAppData({
       settings: { ...app.settings, 
-        flow,
+        flow: activeUser.flow,
         kybHandle: activeUser && activeUser.business ? activeUser.handle : false,
         kybAdminHandle: activeUser && activeUser.admin ? activeUser : activeUser && activeUser.business && !activeUser.certified && app.users.some(u => u.admin && u.business_handle === activeUser.handle) ? app.users.find(u => u.admin && u.business_handle === activeUser.handle).handle : false
       },
-      users: app.users.map(({ active, ...u }) => u.handle === handle ? { ...u, active: true, flow } : u)
+      users: app.users.map(({ active, ...u }) => u.handle === handle ? { ...u, active: true } : u)
     }, () => {
       updateApp({ activeUser });
-      history.push({ pathname: handleHomeRedirect(app, flows, flow, handle), state: { from: location.pathname } });
+      console.log(activeUser.flow);
+      history.push({ pathname: handleHomeRedirect(app, flows, activeUser.flow, handle), state: { from: location.pathname } });
     });
   };
 
@@ -41,7 +41,7 @@ const NavbarUsers = () => {
       onChange={setActiveUser}
       className="text-nowrap users"
       value={app.activeUser ? (app.settings.kybAdminHandle || app.activeUser.handle) : undefined}
-      options={app.users.filter(u => !u.business_handle).map(user => ({ label: user.handle, value: user.handle, htmlBefore: user.business ? <img src={businessIcon} width={16} height={16} alt="Business" className="mt-n1 mr-2" /> : 
+      options={app.users.filter(u => !u.business_handle && !app.settings.kybAdminHandle || app.settings.kybAdminHandle && app.settings.kybHandle !== u.handle).map(user => ({ label: user.handle, value: user.handle, htmlBefore: user.business ? <img src={businessIcon} width={16} height={16} alt="Business" className="mt-n1 mr-2" /> : 
       <img src={indvidualIcon} width={16} height={16} alt="Individual" className="mt-n1 mr-2" /> }))} />
     <Button as={NavLink} to="/" onClick={() => setNewUser(() => history.push('/'))} className="ml-2 text-nowrap" size="sm"><i className="fas fa-user-plus text-lg text-white"></i><span className="ml-2 d-none d-sm-inline">New Entity</span></Button>
   </div>;
